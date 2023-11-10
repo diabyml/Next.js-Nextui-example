2ceb4b828792e0e768377edf399e9ccc8d07ce8b
diabyml <diaby223.ml@gmail.com>
2023-11-10 10:15:47 +0000

    added next-theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import { ThemeSwitcher } from "@/components/themeSwitcher";
 import { Button } from "@nextui-org/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="p-5">
+      <ThemeSwitcher />
       <h1>Hello, world</h1>
       <Button color="primary">Save Changes</Button>
     </div>
